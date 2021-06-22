ea1fc9995fd01c93a79a1cf9a910ddfa99feea25
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2021-06-22 11:19:26 +0530

    Update Dice_Roller.py

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -2,3 +2,5 @@ import random
 while True:     
      print(''' 1. roll the dice             2. exit     ''')    
      user = int(input("what you want to do\n"))
+     if user==1:         
+        number = random.randint(1,6)
